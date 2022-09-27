08d7d74513a64663dfb87a8917d0eaba92330450
AKIRA <602646397@qq.com>
2022-09-27 13:51:59 +0800

    Android打包keystore设置文件（暂时不用，注释状态）

---
+++ Assets/Scripts/Editor/GlobalConfig.cs
@@ -0,0 +1,13 @@
+// using UnityEditor;
+
+// [InitializeOnLoad]
+// public class GlobalConfig {
+//     /// <summary>
+//     /// https://blog.csdn.net/AcmHonor/article/details/94552089?spm=1001.2101.3001.6650.2&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ETopNSimilar%7Edefault-2-94552089-blog-107976466.topnsimilarv1&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ETopNSimilar%7Edefault-2-94552089-blog-107976466.topnsimilarv1&utm_relevant_index=5
+//     /// </summary>
+//     static GlobalConfig() {
+//         PlayerSettings.Android.keystorePass = "180513";
+//         PlayerSettings.Android.keyaliasName = "key0";
+//         PlayerSettings.Android.keyaliasPass = "180513";
+//     }
+// }
