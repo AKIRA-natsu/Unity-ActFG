f665da8e029c501f03cfe81700a7beede9b86875
AKIRA <602646397@qq.com>
2022-11-10 10:35:46 +0800

    修改FloatRange

--- Assets/Scripts/Editor/Drawer/FloatRangeDrawer.cs
+++ Assets/Scripts/Editor/Drawer/FloatRangeDrawer.cs
@@ -10,20 +10,35 @@ public class FloatRangeDrawer : PropertyDrawer
     //label使用的默认标签
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        EditorGUI.BeginProperty(position, label, property);
+        var minProp = property.FindPropertyRelative("min");
+        var maxProp = property.FindPropertyRelative("max");
 
-        //显示字段标签，返回标签占用后剩余的区域
-        position = EditorGUI.PrefixLabel(position, label);
-        //区域宽度设为一半
-        position.width = position.width / 2f;
-        //调整标签Label宽度为 当前宽度的一半
-        EditorGUIUtility.labelWidth = position.width / 2f;
-        EditorGUI.PropertyField(position, property.FindPropertyRelative("min"));
-        //max属性右移
-        position.x += position.width;
+        if (Application.isPlaying) {
+            EditorGUI.BeginProperty(position, label, property);
 
-        EditorGUI.PropertyField(position, property.FindPropertyRelative("max"));
+            //显示字段标签，返回标签占用后剩余的区域
+            position = EditorGUI.PrefixLabel(position, label);
+            var valueProp = property.FindPropertyRelative("value");
+            valueProp.floatValue =
+                EditorGUI.Slider(position, valueProp.floatValue, minProp.floatValue, maxProp.floatValue);
 
-        EditorGUI.EndProperty();
+            EditorGUI.EndProperty();
+        } else {
+            EditorGUI.BeginProperty(position, label, property);
+
+            //显示字段标签，返回标签占用后剩余的区域
+            position = EditorGUI.PrefixLabel(position, label);
+            //区域宽度设为一半
+            position.width = position.width / 2f - 5f;
+            //调整标签Label宽度为 当前宽度的一半
+            EditorGUIUtility.labelWidth = position.width / 2f - 20f;
+            EditorGUI.PropertyField(position, minProp);
+            //max属性右移
+            position.x += position.width + 10f;
+
+            EditorGUI.PropertyField(position, maxProp);
+
+            EditorGUI.EndProperty();
+        }
     }
 }
--- Assets/Scripts/Other/FloatRange.cs
+++ Assets/Scripts/Other/FloatRange.cs
@@ -3,9 +3,32 @@
 [System.Serializable]
 public struct FloatRange
 {
+    /// <summary>
+    /// 边界
+    /// </summary>
     public float min, max;
-    
-    public float RangeValueInRange
+
+    /// <summary>
+    /// <para>滑动值</para>
+    /// <para>PropertyDrawer</para>
+    /// </summary>
+    public float value;
+    /// <summary>
+    /// 滑动值
+    /// </summary>
+    /// <value></value>
+    public float Value {
+        get => value;
+        set {
+            this.value = Mathf.Clamp(value, min, max);
+        }
+    }
+
+    /// <summary>
+    /// 随机值
+    /// </summary>
+    /// <value></value>
+    public float RandomValue
     {
         get
         {
