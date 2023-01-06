1034b2a4d01fce36d3e3307e71c75e0e878dc7c9
AKIRA <602646397@qq.com>
2023-01-06 10:40:55 +0800

    Camera表现基类修改

--- Assets/Scripts/Behaviour/Camera/CameraBehaviour.cs
+++ Assets/Scripts/Behaviour/Camera/CameraBehaviour.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 /// <summary>
 /// 摄像机表现基类
 /// </summary>
+[DefaultExecutionOrder(1)]
 public abstract class CameraBehaviour : MonoBehaviour, IUpdate {
     // 更新模式
     public UpdateMode mode = UpdateMode.Update;
 
     private void OnEnable() {
-        UpdateManager.Instance.Regist(this, mode);
+        this.Regist(mode);
     }
 
     private void OnDisable() {
-        if (!UpdateManager.IsApplicationOut)
-            UpdateManager.Instance.Remove(this, mode);
+        this.Remove(mode);
     }
 
     public abstract void GameUpdate();
