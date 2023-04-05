2f7d23c47a7aaf15aefc1463a381389e36b493c7
AKIRA <602646397@qq.com>
2023-04-05 22:42:22 +0800

    1.删除不用的Package，升级部分Package版本 2.添加一些武器的模型和音效

---
+++ Assets/Assets/Prefabs/Weapon/Low Poly Guns/Scripts/GunsMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class GunsMenu : MonoBehaviour
+{
+    public GameObject Buttons;
+    public GameObject[] Guns;
+    int currentGun = 0;
+    void Start()
+    {
+        Guns[0].SetActive(true);
+    }
+
+    public void NextGun()
+    {
+        Guns[currentGun ].SetActive(false);
+        currentGun++;
+        if (currentGun >= Guns.Length)
+            currentGun = 0;
+        Guns[currentGun].SetActive(true);
+    }
+    public void PreviousGun()
+    {
+        Guns[currentGun].SetActive(false);
+        currentGun--;
+        if (currentGun < 0)
+            currentGun = Guns.Length - 1;
+        Guns[currentGun].SetActive(true);
+    }
+    private void Update()
+    {
+        if ((Mouse.current.leftButton.wasPressedThisFrame && !EventSystem.current.IsPointerOverGameObject())
+            //  || (Input.touchCount > 0 && Input.GetTouch(0).phase == UnityEngine.TouchPhase.Began && !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+        )
+        {
+            Buttons.SetActive(false);
+        }
+        else if(!Mouse.current.leftButton.isPressed
+            // && Input.touchCount == 0
+        )
+        {
+            Buttons.SetActive(true);
+        }
+    }
+}
